ba0997a62a3f7e8e8f75c6cfeca8a26235109263
Jeng-Sheng Yeh <jsyeh@mail.mcu.edu.tw>
2021-03-08 16:20:42 +0800

    Add files via upload

---
+++ week03-2_glut_copy_paste.cpp
@@ -0,0 +1,26 @@
+///留下精華
+#include <GL/glut.h> ///(1) 使用 GLUT外掛
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            glBegin(GL_TRIANGLES);
+
+                glColor3f(1.0f, 0.0f, 0.0f);   glVertex2f(0.0f,   1.0f);
+                glColor3f(0.0f, 1.0f, 0.0f);   glVertex2f(0.87f,  -0.5f);
+                glColor3f(0.0f, 0.0f, 1.0f);   glVertex2f(-0.87f, -0.5f);
+
+            glEnd();
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[]) ///我們只要留 main()裡面的 5行
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("GLUT Shapes");
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+}
---
+++ week03-4_argc_argv.c
@@ -0,0 +1,10 @@
+///把第1個範例 OpenGL, 拿來亂改
+///把第1個Project變 *粗黑色* Active
+#include <stdio.h>
+int main( int argc, char** argv)
+{
+    printf("現在的 argc是:%d\n", argc);
+    for(int i=0; i<argc; i++ ){
+        printf("argv[%d]是:%s\n", i, argv[i] );
+    }
+}
